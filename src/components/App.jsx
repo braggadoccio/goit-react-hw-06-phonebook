a7feda01759b20af40917a465ce8d7658593c8b8
Paulo <pau.solanoy@gmail.com>
2024-03-01 08:55:45 +0800

    update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
+import { useState, useEffect } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+const initialContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
 export const App = () => {
+  const savedContacts = localStorage.getItem('contacts');
+  const [contacts, setContacts] = useState(
+    savedContacts !== null ? JSON.parse(savedContacts) : initialContacts
+  );
+  const [filter, setFilter] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
+
+  const addContact = newContact => {
+    setContacts(prevContact => [...prevContact, newContact]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevContact =>
+      prevContact.filter(contact => contact.id !== id)
+    );
+  };
+
+  const filterContact = () => {
+    const filterLowerCase = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterLowerCase)
+    );
+  };
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <h1 className="title">Phonebook</h1>
+      <ContactForm addContact={addContact} contacts={contacts} />
+
+      <h2 className="title">Contacts</h2>
+      <Filter filter={filter} setFilter={setFilter} />
+      <ContactList
+        filterContact={filterContact}
+        deleteContact={deleteContact}
+      />
     </div>
   );
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,27 @@
+import css from './Filter.module.css';
+import PropTypes from 'prop-types';
+
+export const Filter = ({ filter, setFilter }) => {
+  const handleFilterChange = e => {
+    setFilter(e.target.value);
+  };
+
+  return (
+    <div className={css.container}>
+      <p className={css.title}>Find Contacts by Name</p>
+      <input
+        type="text"
+        name="filter"
+        placeholder="Search by name"
+        value={filter}
+        onChange={handleFilterChange}
+        className={css.input}
+      />
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  filter: PropTypes.string.isRequired,
+  setFilter: PropTypes.func.isRequired,
+};
